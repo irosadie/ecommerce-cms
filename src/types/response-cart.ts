c5c4d8a9323da014160a69246130b55ebf82e09a
bangimron <imron.rosadi@jatis.com>
2023-08-03 14:01:32 +0700

    add some types

---
+++ src/types/response-cart.ts
@@ -0,0 +1,26 @@
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+  total: number;
+  discountPercentage: number;
+  discountedPrice: number;
+}
+
+export interface GetCartResponse {
+  id: number;
+  products: Product[];
+  total: number;
+  discountedTotal: number;
+  userId: number;
+  totalProducts: number;
+  totalQuantity: number;
+}
+
+export interface GetAllCartsResponse {
+  carts: GetCartResponse[];
+  total: number;
+  skip: number;
+  limit: number;
+}
